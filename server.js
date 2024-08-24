df7b88e5a3a04458f28733677465f1a952126c35
SeniorJunior123 <hiepbo100@gmail.com>
2024-08-24 22:52:32 +0700

    express helloworld

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const { createServer } = require('node:http');//node.js
+// import express from 'express';
 
-const hostname = 'localhost';//localhost
-const port = 3000;
+const express = require('express'); //commonjs
+const app = express(); //app express
+const port = 8081; //port
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
