7adad839be9e7171b4d8152abaf7bafe622be864
SeniorJunior123 <hiepbo100@gmail.com>
2024-08-24 23:35:40 +0700

    thay doi

---
+++ src/routes/web.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+router.get('/abc', (req, res) => {
+    res.send('Hello World!')
+})
+
+router.get('/demohtml', (req, res) => {
+    // res.send('Hello World!')
+    res.render('sample.ejs')
+})
+
+module.exports = router;
